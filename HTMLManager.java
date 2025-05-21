105565bb63eb5d78a33eedb960499bc0e4ab1856
matthewoo527 <matthewoo527@gmail.com>
2025-05-21 11:45:46 -0700

    Add files via upload

--- HTMLManager.java
+++ HTMLManager.java
@@ -2,20 +2,28 @@ import java.util.*;
 
 public class HTMLManager {
   private Queue<HTMLTag> tags;
+  
   public HTMLManager(Queue<HTMLTag> html){
-      
-       
       if (html== null){
          throws new illegalArgumentException;
-    
       }else{
          tags= new LinkedList<HTMLTag>();
          for(HTMLTag tag: html){
            tags.add(tag);
          }
       }
-      
+  }    
   
+  public Queue<HTMLTag> getTags(){
+      return tags ;  
   }
   
+  public String toString(){
+   String newTag=""
+   for(HTMLTag tag: tags){
+      newTag+=tag;
+   }
+   return newTag.trim();
+  }
+    
 }
